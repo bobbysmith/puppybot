91b5c205efa1f99254954d12cba6c6d08e3d45c4
Bobby Smith <bobbysmith24@gmail.com>
2015-10-19 10:14:15 -0500

    getting puppy url

--- puppybot.js
+++ puppybot.js
@@ -4,21 +4,23 @@ var requestify = require('requestify');
 module.exports = function (req, res, next) {
 
   var puppyUrl;
+
   requestify.get('http://www.thepuppyapi.com/puppy').then(function (response) {
     response.getBody();
 
     var resBody = JSON.parse(response.body);
     puppyUrl = resBody.puppy_url;
     console.log(puppyUrl);
+    
+    var username = req.body.user_name;
+    var botPayload = { text: 'hello @' + username + ' ' + puppyUrl};
+    console.log(botPayload);
+
+    if (username !== 'slackbot') {
+      return res.status(200).json(botPayload);
+    } else {
+      return res.status(200).end();
+    }
   });
 
-  var username = req.body.user_name;
-
-  var botPayload = { text: 'hello @' + username + ' ' + puppyUrl};
-
-  if (username !== 'slackbot') {
-    return res.status(200).json(botPayload);
-  } else {
-    return res.status(200).end();
-  }
 }
